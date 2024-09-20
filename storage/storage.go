6d0c104df89b6412da29b650aa4dd6476f1865e9
Helios <79072303+Helios-fr@users.noreply.github.com>
2024-09-20 11:04:50 +1000

    ⚙️ CODE : Updated storage.go

--- storage/storage.go
+++ storage/storage.go
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/download/", downloadHandler)
 
 	fmt.Println("Server started at :9002")
-	http.ListenAndServe(":9002", nil)
+	http.ListenAndServe("0.0.0.0:9002", nil)
 }
 
 func generateRandomID() (string, error) {
