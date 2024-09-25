cead3335bcf38edd0833304f6b37281e6842c4c5
Helios <79072303+Helios-fr@users.noreply.github.com>
2024-09-25 12:38:06 +1000

    ⚙️ CODE : Finished navbar desktop

--- frontend/src/routes/mocks/landing/navbar_desktop.svelte
+++ frontend/src/routes/mocks/landing/navbar_desktop.svelte
@@ -1,90 +1,37 @@
 <script>
 	import logo from '$lib/assets/images/logo.png';
-    import wallet from '$lib/assets/images/wallet_connect.png';
 
 	export let Navbar_Desktop;
 </script>
 
-<nav class="navbar w-full p-4">
+<nav class="navbar w-full p-4 bg-base-200">
 	<div class="navbar-start flex-grow">
 		<a class="btn btn-ghost" href="#">
 			<img src={logo} alt="Logo" class="h-12" />
-			<p class="text-4xl font-bold mb-1">Geode</p>
+			<p class="text-4xl font-bold">Geode</p>
 		</a>
 		
-		<div class="relative group">
-			<a class="btn btn-ghost" href="#features">
-				<p class="text-2xl font-bold">Sell</p>
-			</a>
-			<div class="absolute hidden group-hover:block">
-				<div class="card card-side card-compact w-128 bg-primary shadow">
-					<div class="card-body">
-						<h2 class="card-title text-primary-content text-xl">Sell</h2>
-						<p class="text-primary-content">Sell your files, safely and securely, with end-to-end encryption.</p>
-					</div>
-					<figure class="w-64 h-full">
-						<img src="https://placehold.co/256x256" class="h-full object-cover" />
-					</figure>
-				</div>
-			</div>
-		</div>
+		<a class="btn btn-ghost" href="#features">
+			<p class="text-xl font-bold ">Sell</p>
+		</a>
 		
-		<div class="relative group">
-			<a class="btn btn-ghost" href="#features">
-				<p class="text-2xl font-bold">Store</p>
-			</a>
-			<div class="absolute hidden group-hover:block">
-				<div class="card card-side card-compact w-128 bg-primary shadow">
-					<div class="card-body">
-						<h2 class="card-title text-primary-content text-xl">Store</h2>
-						<p class="text-primary-content">Store your files securely and access them from anywhere, anytime.</p>
-					</div>
-					<figure class="w-64 h-full">
-						<img src="https://placehold.co/256x256" class="h-full object-cover" />
-					</figure>
-				</div>
-			</div>
-		</div>
+		<a class="btn btn-ghost" href="#features">
+			<p class="text-xl font-bold">Store</p>
+		</a>
 		
-		<div class="relative group">
-			<a class="btn btn-ghost" href="#features">
-				<p class="text-2xl font-bold">Pay</p>
-			</a>
-			<div class="absolute hidden group-hover:block">
-				<div class="card card-side card-compact w-128 bg-primary shadow">
-					<div class="card-body">
-						<h2 class="card-title text-primary-content text-xl">Pay</h2>
-						<p class="text-primary-content">Get paid instantly for your services, without revealing your identity.</p>
-					</div>
-					<figure class="w-64 h-full">
-						<img src="https://placehold.co/256x256" class="h-full object-cover" />
-					</figure>
-				</div>
-			</div>
-		</div>
 		
-		<div class="relative group">
-			<a class="btn btn-ghost" href="#features">
-				<p class="text-2xl font-bold">Keys</p>
-			</a>
-			<div class="absolute hidden group-hover:block">
-				<div class="card card-side card-compact w-128 bg-primary shadow">
-					<div class="card-body">
-						<h2 class="card-title text-primary-content text-xl">Keys</h2>
-						<p class="text-primary-content">Manage who can access your products and services, simply and securely.</p>
-					</div>
-					<figure class="w-64 h-full">
-						<img src="https://placehold.co/256x256" class="h-full object-cover" />
-					</figure>
-				</div>
-			</div>
-		</div>
+		<a class="btn btn-ghost" href="#features">
+			<p class="text-xl font-bold">Pay</p>
+		</a>
+		
+		<a class="btn btn-ghost" href="#features">
+			<p class="text-xl font-bold">Keys</p>
+		</a>
 	</div>
-	
+
 	<div class="navbar-end flex-shrink" style="width: 0;">
-		<a class="btn" href="#">
-			<img src={wallet} alt="Wallet Connect" class="h-12" />
-			<p class="text-xl font-bold">Connect Wallet</p>
+		<a class="btn bg-gradient-to-r from-primary to-accent" href="#">
+			<p class="text-xl font-bold text-base-200">Connect Wallet</p>
 		</a>
 	</div>
 </nav>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,31 +7,41 @@ module.exports = {
   theme: {
     screens: {
       'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
       'md': '800px',
-      // => @media (min-width: 768px) { ... }
-
       'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
       'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    }
+    },
+    fontFamily: {
+      // Use verdana as the default font
+      sans: ['Verdana', 'sans-serif'],
+      // Use roboto as the default monospace font
+      mono: ['Roboto Mono', 'monospace'],
+    },
   },
   daisyui: {
     themes: [
       catppuccin('macchiato'),
       {geode: {
         "primary": "#ecabde",
+        "primary-content": "#130b12",
         "secondary": "#af69b1",
-        "accent": "#8CC",
+        "secondary-content": "#0b040c",
+        "accent": "#8cc",
+        "accent-content": "#060f0f",
         "neutral": "#2e2d3e",
+        "neutral-content": "#d1d1d5",
         "base-100": "#2e2d3e",
+        "base-200": "#272635",
+        "base-300": "#1f1f2b",
+        "base-content": "#d1d1d5",
         "info": "#8AADF4",
+        "info-content": "#070b14",
         "success": "#A6DA95",
-        "warning": "#EED49F",
+        "success-content": "#0a1108",
+        "warning": "#EED49f",
+        "warning-content": "#141009",
         "error": "#ED8796",
+        "error-content": "#140608",
       },},
     ],
   },
